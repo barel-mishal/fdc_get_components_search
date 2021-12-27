db694ca0eed3d899f6071f7d6eafe735ebd17c09
dreamwork-Barel-and-Yuval <dreamwork@dreamwork.network>
2021-12-27 11:07:16 +0200

    fix url

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const axios = require('axios');
-const path = require('path');
+
 
 const { nuterientList } = require('./nutrients.js');
 
 const app = express();
 
-PORT = process.env.PORT || 3002
+PORT = process.env.PORT || 3003
 
 // https://fdc.nal.usda.gov/portal-data/external/nutrientList/search
 
@@ -19,24 +19,31 @@ const parms = (id) => ({
       "foodTypes":["Foundation","Survey (FNDDS)","SR Legacy"]
       });
 
-app.use("/", express.static(path.join(__dirname, 'fdcSearcherSite', 'index.html')))
+
+// app.get('/', function (req, res) {
+//   express.static()
+//   res.sendFile('/', path.join(__dirname, 'fdcSearcherSite', 'index.html'))
+// });
+
 
 app.get('/search', async (req, res, next) => {
   const { qConponent } = req.query
+  console.log(qConponent);
   const { id } = nuterientList.find((obj) => `${obj.name} (${obj.nutrientUnit.name})` === qConponent)
   try {
     const response = await axios.post('https://fdc.nal.usda.gov/portal-data/external/nutrientList/search', parms(id));
     res.send(response.data)
+    console.log(response);
   }
   catch(e) {
     next(e)
   }
-})
+});
 
 app.listen(PORT, (err) => {
   if (err) {
     console.error(err)
     return 
   } 
-  console.log('google')
-})
+  console.log('listening on port ', `${PORT}`)
+});
